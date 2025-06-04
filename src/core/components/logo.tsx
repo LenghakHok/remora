54754e5bbcdff7981ac0afc17301ea8c50cb5327
Lenghak Hok <lenghakhok.dev@gmail.com>
2025-06-04 16:30:11 +0700

    style(css): changed the themes to lime

--- src/core/components/logo.tsx
+++ src/core/components/logo.tsx
@@ -1,4 +1,4 @@
-import { BrandIconDark, BrandIconLight } from "@/core/icons/brand";
+import { BrandIcon } from "@/core/icons/brand";
 import { cn } from "@/core/lib/cn";
 import { buttonVariants } from "@/core/ui/button";
 import type { ComponentPropsWithRef } from "react";
@@ -14,8 +14,7 @@ export function Logo({ className, ...props }: ComponentPropsWithRef<"a">) {
       href="/"
       {...props}
     >
-      <BrandIconDark className="hidden dark:block" />
-      <BrandIconLight className="block dark:hidden" />
+      <BrandIcon />
       <span className="sr-only"> Logo </span>
     </a>
   );
